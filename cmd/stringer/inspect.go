81d9a1ab18f83867ace6d3dd916bfac559561ac0
haining.zhang <haining.zhang@grabtaxi.com>
2023-03-02 19:50:33 +0800

    learn cobra

---
+++ cmd/stringer/inspect.go
@@ -0,0 +1,32 @@
+package stringer
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"github.com/wncbb/stringer/pkg/stringer"
+)
+
+var onlyDigits bool
+var inspectCmd = &cobra.Command{
+	Use:     "inspect",
+	Aliases: []string{"insp"},
+	Short:   "Inspects a string",
+	Args:    cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+
+		i := args[0]
+		res, kind := stringer.Inspect(i, onlyDigits)
+
+		pluralS := "s"
+		if res == 1 {
+			pluralS = ""
+		}
+		fmt.Printf("'%s' has a %d %s%s.\n", i, res, kind, pluralS)
+	},
+}
+
+func init() {
+	inspectCmd.Flags().BoolVarP(&onlyDigits, "digits", "d", false, "Count only digits")
+	rootCmd.AddCommand(inspectCmd)
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "github.com/wncbb/stringer/cmd/stringer"
+
+// https://www.thorsten-hans.com/lets-build-a-cli-in-go-with-cobra/
+
+func main() {
+	stringer.Execute()
+}
